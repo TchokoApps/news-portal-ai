0c09ea7925981bb239f0b03674cae1f6cb8cbdf1
tchokoapps <tchokoapps@gmail.com>
2026-04-10 21:45:07 +0200

    new changes mades

---
+++ resources/views/admin/layouts/footer.blade.php
@@ -0,0 +1,8 @@
+      <footer class="main-footer">
+        <div class="footer-left">
+          Copyright &copy; 2026 <div class="bullet"></div> Design By <a href="https://tchokoapps.com/">TchokoApps</a>
+        </div>
+        <div class="footer-right">
+
+        </div>
+      </footer>
